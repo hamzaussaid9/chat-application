e53d61d174a11b684a12947d423064016b41f85b
hamza.ussaid <hamza.ussaid@confiz.com>
2023-03-27 02:28:14 +0500

    added home route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Authlayout from './components/layout/Authlayout';
 import Layout from './components/layout/Layout';
+import Home from './pages/Home';
 import Auth from './pages/Auth';
 import PageNotFound from './pages/PageNotFound';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<Home />} path="/" />
         <Route element={<Authlayout />}>
           <Route element={<Auth />} path="/auth" />
         </Route>
